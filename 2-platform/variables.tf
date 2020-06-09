c493842dceda5e67aa4c75b60e7f1bac74225a0b
aiya44 <ychen0404@icloud.com>
2020-06-09 14:56:39 -0700

    terraform platform

--- 2-platform/variables.tf
+++ 2-platform/variables.tf
@@ -3,9 +3,15 @@ variable "region" {
   description = "AWS Region"
 }
 
-variable "remote_state_bucket" {
+variable "remote_state_bucket" {}
+variable "remote_state_key" {}
+variable "ecs_cluster_name" {}
+
+variable "internet_cidr_blocks" {
+
 }
 
-variable "remote_state_key" {
+variable "ecs_domain_name" {
+  
 }
 
