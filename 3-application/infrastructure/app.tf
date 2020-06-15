2b6b86df4e87b07a626e195fa15584da5f222a3e
aiya44 <ychen0404@icloud.com>
2020-06-15 09:38:26 -0700

    alb

--- 3-application/infrastructure/app.tf
+++ 3-application/infrastructure/app.tf
@@ -78,10 +78,63 @@ resource "aws_iam_role_policy" "fargate_iam_role_policy" {
     EOF
 }
 
-#security group for the application on fargate 
+#security group for the application on fargate
 
 resource "aws_security_group" "app_security_group" {
     name = "${var.ecs_service_name}-SG"
     description = "Security group for springbootapp to communicate in and out"
     vpc_id = data.terraform_remote_state.platform.vpc_id
+#passing in the port config for the actual spring boot application
+#this appows application to fo out to the internet freely without any limitations of ports or protocols
+# 
+    ingress {
+        from_port = 8080
+        protocol = "TCP"
+        to_port = 8080
+        cidr_blocks = [data.terraform_remote_state.platform.vpc_cidr_blocks]
+    }
+
+    egress {
+        from_port = 0
+        protocol = "-1"
+        to_port = 0 
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "${var.ecs_service_name}-SG"
+
+    }
 }
+
+#creating alb target group for the ecs-fargate tasks where we can use it for the application load balalncer. 
+
+
+resource "aws_alb_target_group" "ecs_app_target_group" {
+    name = "${var.ecs_service_name}-TG"
+    port = "${var.docker_container_port}"
+    protocol = "HTTP"
+    vpc_id = "${data.terraform_remote_state.platform.vpc_id}"
+    target_type = "ip"
+  
+    #providing a health check mechanism for the application so that EC@ target groups can 
+    #understand that the application is healthy and then fargate can reconize 
+    health_check{
+        path = "/actuator/health"
+        protocol = "HTTP"
+        matcher = "200"
+        #intervals for the check 
+        interval = 60
+        timeout = 30
+        #after 3 re-tries of unhealthy application health checks it is going to go down and deregister from this target group
+        unhealthy_threshold = "3"
+        healthy_threshold = "3"
+    }
+    
+    tags {
+        Name = "${var.ecs_service_name}-TG"
+    }
+
+}
+
+#creating ecs service 
