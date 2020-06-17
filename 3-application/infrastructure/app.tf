06b2f42740ee567a55c7a3b05fa0963502c34501
aiya44 <ychen0404@icloud.com>
2020-06-17 12:46:42 -0700

    added Dockerfile & deploy.sh file

--- 3-application/infrastructure/app.tf
+++ 3-application/infrastructure/app.tf
@@ -31,14 +31,15 @@ data "template_file" "ecs_task_defintion_template" {
 }
 
 resource "aws_ecs_task_definition" "springbootapp-task-defintion" {
+    # using the render argument in the task defintion 
     container_defintions = data.template_file.ecs_task_defintion_template.rendered
     family = var.ecs_service_name
     cpu = 512
     memory = var.memory
     requires_compatibilities = ["FARGATE"]
     networking_mode = "awsvpc"
-    execution_role_arn = ""
-    task_role_arn = ""
+    execution_role_arn = aws_iam_role.fargate_iam_role.arn
+    task_role_arn = aws_iam_role.fargate_iam_role.arn
 }
 
 #use json inline document
